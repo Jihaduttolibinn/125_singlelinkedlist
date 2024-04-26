a6409971f62f4dd38e12efc7779b0db343c966ab
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-04-26 11:29:16 +0800

    memasukkan void traverse

--- 125_singlelinkedlist/125_singlelinkedlist.cpp
+++ 125_singlelinkedlist/125_singlelinkedlist.cpp
@@ -86,6 +86,23 @@ void addNode() {
 
 	}
 
+	void traverse() {
+		if (listEmpty()) {
+			cout << "List Kosong" << endl;
+			system("pause");
+			system("cls");
+			return;
+		}
+		else {
+			Node* currentNode = START;
+			while (currentNode != NULL) {
+				cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+				currentNode = currentNode->next;
+			}
+		}
+	}
+	
+
 	
 
 
