d3c22aca255fc8cfcd2365f8b962f95750fb1bf5
jihaduttolibinn <jihaduttolibin02@gmail.com>
2024-04-26 11:27:20 +0800

    Memasukkan bool deleteNode

--- 125_singlelinkedlist/125_singlelinkedlist.cpp
+++ 125_singlelinkedlist/125_singlelinkedlist.cpp
@@ -68,7 +68,19 @@ void addNode() {
 		return false;	// step 4
 	}
 }
+	bool deleteNode(int nim) {
+		Node* current = START;
+		Node* previous = START;
+		if (serachNode(nim, previous, current) == false)
+			return false;
+		previous->next = current->next;
+		if (current == START)
+			START = current->next;
+		return true;
+	}
+
 	
+
 
 
 
